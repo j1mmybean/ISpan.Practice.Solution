8984caf84ede8a648d366d4e3478d5eb9f2b6e77
j1mmybean <carb4305@gmail.com>
2023-02-19 16:33:47 +0800

    test update

--- ISpan.Practice.Tests/CalculatorExtensionsTests.cs
+++ ISpan.Practice.Tests/CalculatorExtensionsTests.cs
@@ -4,6 +4,16 @@ namespace ISpan.Practice.Tests
 {
 	public class CalculatorExtensionsTests
 	{
+		[TestCase("-12+33-444*56/7")] // Attribute特徵項
+		public void GetNumbers_傳回decimal數字List(string formula)
+		{
+			string source = formula;
+			var expected = new List<decimal> { -12, 33, 444, 56, 7 };
+
+			var actual = source.GetNumbers();
+			Assert.AreEqual(expected, actual);
+		}
+
 		[Test]
 		public void GetOperators_傳回加減乘除字串List()
 		{
@@ -14,33 +24,25 @@ namespace ISpan.Practice.Tests
 			Assert.AreEqual(expected, actual);
 		}
 
-		[TestCase("-12+33-444*56/7")] // Attribute特徵項
-
-		public void GetNumbers_傳回decimal數字List(string formula)
+		public void MultiplcationAndDivision_傳回加減乘除字串List()
 		{
-			string source = formula;
-			var expected = new List<decimal> { -12, 33, 444, 56, 7 };
+			var numbersSource = new List<decimal> { -12, 33, 444, 56, 7 };
+			var operatorsSource = new List<string> { "+", "-", "*", "/" };
 
-			var actual = source.GetNumbers();
+			var expected = (new List<decimal> { 12, 33, 444 * 56 / 7 }, new List<string> { "+", "-" });
+
+			var actual = (numbersSource.MultiplcationAndDivision(operatorsSource, out operatorsSource), operatorsSource);
 			Assert.AreEqual(expected, actual);
 		}
-		//public void MultiplcationAndDivision_傳回加減乘除字串List()
-		//{
-		//	string source = "-12+33-444*56/7";
-		//	var expected = new List<decimal> { 12, 33, 444, 56, 7 };
+		public void AdditionAndSubstraction_輸入數字List和運算子List_傳回計算結果()
+		{
+			var numbersSource = new List<decimal> { 12, 33, 444 * 56 / 7 };
+			var operatorsSource = new List<string> { "+", "-" };
 
-		//	var actual = source.GetNumbers();
-		//	Assert.AreEqual(expected, actual);
-		//}
-		//public void AdditionAndSubstraction_輸入數字List和運算子List_傳回計算結果()
-		//{
-		//	var numbers = new List<decimal> { 12, 33, 444, 56, 7 };
-		//	var operators = new List<string> { 12, 33, 444, 56, 7 };
+			var expected = numbersSource.AdditionAndSubstraction(operatorsSource);
 
-		//	var expected = new List<decimal> { 12, 33, 444, 56, 7 };
-
-		//	var actual = source.GetNumbers();
-		//	Assert.AreEqual(expected, actual);
-		//}
+			var actual = 12 + 33 - 444 * 56 / 7;
+			Assert.AreEqual(expected, actual);
+		}
 	}
 }
